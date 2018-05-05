eec71331b5a0784291bb185f718b691bcd2f3f6a
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-05-05 14:55:37 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,20 @@
+def instantiate_new_array
+  return []
+end 
+
+def array_with_two_elements
+  return ["",""]
+end
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array[-1]
+end 
+
